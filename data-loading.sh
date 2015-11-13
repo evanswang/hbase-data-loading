6ccec05d43ac2cdce41d0ac35d1247e4a52b5336
evanswang <evanswang@ymail.com>
2015-11-13 15:54:58 +0000

    1

--- data-loading.sh
+++
@@ -1,9 +0,0 @@
-if [ -n $((sh/install_jdk.sh) 1>&2)"" ]; then
-	echo "***FINAL DECISION***: please install jdk-7 manually and run this script again"
-	exit
-fi
-
-if [ -n $((sh/deploy_hbase.sh) 1>&2)"" ]; then
-	echo "***FINAL DECISION***: please install hbase 0.98.0 hadoop2 manually and run this script again"
-	exit
-fi
